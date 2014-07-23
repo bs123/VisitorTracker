70dd8bc36b0b5e7a86ce9852f46d5fcee3dd1833
j <bs123@example.com>
2014-07-23 13:37:11 +0200

    Sending only part of the data

--- tracker.js
+++ tracker.js
@@ -2,6 +2,7 @@ console.log("VisitorTracker v0.0")
 
 var noble = require('noble')
 var mqtt  = require('mqtt')
+var util = require('util')
 
 client = mqtt.createClient(1883, 'localhost');
 
@@ -13,7 +14,8 @@ noble.on('stateChange', function(state) {
   }
 });
 
-noble.on('discover', function(peripheral) {
-    client.publish('presence', peripherial);
+noble.on('discover', function(peripherial) {
+  console.log(peripherial.uuid)
+  client.publish('presence', JSON.stringify({uuid: peripherial.uuid, advertisement: peripherial.advertisement, time: new Date().getTime()}))
 });
 
